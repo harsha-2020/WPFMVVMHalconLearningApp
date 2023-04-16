c263690b7f29753a9c5c0cfba81e0d7ee914c8bb
AMV <AMV@AMV-LAPTOP2>
2023-04-16 15:32:32 +1000

    Add project files.

---
+++ WPFMVVMHalconLearning/Models/PrinterChipAnalysisModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HalconDotNet;
+using System;
+using System.IO;
+
+
+namespace WPFMVVMHalconLearning.Models
+{
+	public partial class PrinterChipAnalysisModel : ObservableObject
+	{
+        public HImage Image { get; set; }
+
+		[ObservableProperty]
+		private HRegion _ChipRegion;
+
+		[ObservableProperty]
+		private int _MinGrayValue = 255;
+
+        public PrinterChipAnalysisModel(HImage image)
+        {
+            Image = image;
+        }
+
+        public HRegion ChipRegionProcedure()
+		{
+			string procedurePath = @"..\..\..\HalconProcedures";
+			//MinGrayValue = minGrayValue;
+			HDevEngine Engine = new HDevEngine();
+			Engine.SetProcedurePath(Path.GetFullPath(procedurePath));
+			HDevProcedure procedure = new HDevProcedure("get_printer_chip_regions");
+			HDevProcedureCall ProcedureCall = new HDevProcedureCall(procedure);
+			ProcedureCall.SetInputIconicParamObject("Image", Image);
+			ProcedureCall.SetInputCtrlParamTuple("MinGray", (HTuple)MinGrayValue);
+			ProcedureCall.Execute();
+			HRegion ChipRegion = ProcedureCall.GetOutputIconicParamRegion("ConnectedRegions");
+			Engine.Dispose();
+
+			return ChipRegion;
+		}
+	}
+}
---
+++ WPFMVVMHalconLearning/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HalconDotNet;
+using WPFMVVMHalconLearning.Models;
+
+namespace WPFMVVMHalconLearning.ViewModels
+{
+	public partial class MainViewModel : ObservableObject
+	{
+        public HImage image { get; set; }
+		public HRegion region { get; set; }
+        public MainViewModel()
+        {
+			image = new HImage(@"..\..\..\Images\chipTest.tif");
+			PrinterChipAnalysis = new PrinterChipAnalysisModel(image);
+		}
+
+		[ObservableProperty]
+		[NotifyCanExecuteChangedFor(nameof(ShowRegionCommand))]
+		private PrinterChipAnalysisModel _PrinterChipAnalysis;
+
+		[ObservableProperty]
+		[NotifyCanExecuteChangedFor(nameof(ShowImageCommand))]
+		private HImage _DisplayImage;
+
+		[ObservableProperty]
+		[NotifyCanExecuteChangedFor(nameof(ShowRegionCommand))]
+		private HRegion _DisplayRegion;
+
+		[RelayCommand]
+		public void ShowImage()
+		{
+			DisplayImage = image;
+		}
+
+		[RelayCommand]
+		public void ShowRegion()
+		{			
+			DisplayRegion = PrinterChipAnalysis.ChipRegionProcedure();			
+		}
+	}
+}
